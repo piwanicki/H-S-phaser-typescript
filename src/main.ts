dc448e2057de7412c0bff062dabd28c26f39e0e4
piwanicki <pawel.iwanicki92@gmail.com>
2020-07-10 19:29:24 +0200

    try to make tile from tiled

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,19 @@ import FirstMap from './scenes/FirstMap'
 
 const config: Phaser.Types.Core.GameConfig = {
 	type: Phaser.AUTO,
-	width: '100vw',
-	height: '100vh',
+	width: 800,
+	height: 800,
 	physics: {
 		default: 'arcade',
 		arcade: {
 			gravity: { y:0 }
 		}
 	},
-	scene: [FirstMap]
+	scene: [FirstMap],
+	scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH 
+    },
 }
 
 export default new Phaser.Game(config)
--- src/scenes/FirstMap.ts
+++ src/scenes/FirstMap.ts
@@ -19,33 +19,46 @@ export default class FirstMap extends Phaser.Scene {
         this.load.image('stone', 'assets/images/stone.png');
         this.load.image('hero', 'assets/images/hero.png')
         this.load.image('hero_died', 'assets/images/hero_died.png')
+        this.load.tilemapTiledJSON('map1', 'assets/Map1.json');
+        this.load.image('grassPiskel','assets/grassPiskel.png');
+     
     }
 
     create() {
 
+
         const width = this.scale.width;
         const height = this.scale.height;
 
-        this.background = this.add.tileSprite(width * .5, height * .5, width, height, 'ground')
+        
+        const map = this.make.tilemap({ key: 'map1'})
+        const tilesetMap = map.addTilesetImage('ground','grassPiskel');
+        const ground = map.createStaticLayer('ground',tilesetMap,0,0)
+       // const trees = map.createStaticLayer('trees',tilesetMap,0,0)
+
         this.trees = this.physics.add.staticGroup();
         this.stones = this.physics.add.staticGroup();
 
-        this.player = this.physics.add.sprite(width * .5, height * .5, 'hero').setScale(2);
+        //this.physics.world.setBounds(0,0,width*1.5,height*1.5);
+
+        this.player = this.physics.add.sprite(width * .5, height * .5, 'hero').setScale(3);
+
         // block player to move out from background field
         this.player.setCollideWorldBounds(true);
 
 
-        // generate stones
-        for (let i = 1; i < 6; i++) {
-            const stone = this.stones.create(Phaser.Math.Between(i * (width * .1), i * width * .15), Phaser.Math.Between(height * .1, height), 'stone') as Phaser.Physics.Arcade.Sprite;
-            stone.setScale(2).refreshBody();
-        }
 
-        // generate trees
-        for (let i = 1; i < 10; i++) {
-            const tree = this.trees.create(Phaser.Math.Between(i * (width * .1), i * width * .15), Phaser.Math.Between(height * .1, height), 'tree') as Phaser.Physics.Arcade.Sprite;
-            tree.setScale(3).refreshBody();
-        }
+        // generate stones
+       //for (let i = 1; i < 6; i++) {
+       //    const stone = this.stones.create(Phaser.Math.Between(i * (width * .1), i * width * .15), Phaser.Math.Between(height * .1, height), 'stone') as Phaser.Physics.Arcade.Sprite;
+       //    stone.setScale(2).refreshBody();
+       //}
+
+       //// generate trees
+       //for (let i = 1; i < 10; i++) {
+       //    const tree = this.trees.create(Phaser.Math.Between(i * (width * .1), i * width * .15), Phaser.Math.Between(height * .1, height), 'tree') as Phaser.Physics.Arcade.Sprite;
+       //    tree.setScale(3).refreshBody();
+       //}
 
         // add collisions
         this.physics.add.collider(this.trees, this.player)
@@ -56,7 +69,8 @@ export default class FirstMap extends Phaser.Scene {
 
         // Camera setting
         this.cameras.main.startFollow(this.player);
-        this.cameras.main.setDeadzone(this.scale.width * 1.2, this.scale.height * 1.2);
+        this.cameras.main.setDeadzone(this.scale.width, this.scale.height);
+
     }
 
     update() {
@@ -64,13 +78,13 @@ export default class FirstMap extends Phaser.Scene {
 
         //moving 
         if (this.cursors?.left?.isDown) {
-            this.player?.setVelocityX(-150)
+            this.player?.setVelocityX(-250)
         } else if (this.cursors?.right?.isDown) {
-            this.player?.setVelocityX(150)
+            this.player?.setVelocityX(250)
         } else if (this.cursors?.up?.isDown) {
-            this.player?.setVelocityY(-150)
+            this.player?.setVelocityY(-250)
         } else if (this.cursors?.down?.isDown) {
-            this.player?.setVelocityY(150)
+            this.player?.setVelocityY(250)
         } else {
             this.player?.setVelocity(0);
         }
